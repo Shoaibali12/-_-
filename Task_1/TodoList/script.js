ccdef30bde11b9f29835fa0a9f52e63eba547d97
Shoaibali12 <sind98449@gmail.com>
2024-06-30 09:20:10 -0700

    Todo List Completed

--- Task_1/TodoList/script.js
+++ Task_1/TodoList/script.js
@@ -10,15 +10,30 @@ function adding() {
     alert("please write something");
   } else {
     let li = document.createElement("li");
-
     let del = document.createElement("button");
+    let pend = document.createElement("button");
+
+    pend.className = "pend";
+    pend.textContent = "Pending";
     del.className = "delete";
     del.textContent = "Delete";
     li.textContent = task;
+
     let div = document.createElement("div");
     div.className = "Con";
     div.appendChild(li);
     div.appendChild(del);
+    div.appendChild(pend);
+
+    del.onclick = function () {
+      ulist.removeChild(div);
+    };
+
+    pend.onclick = function () {
+      pend.textContent = "Completed";
+      pend.style.backgroundColor = "green";
+    };
+
     ulist.appendChild(div);
     txtf.value = "";
   }
